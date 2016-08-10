008edc7c808e83c112f3ca6f0bddfc342bbc5adb
Tim G. Thomas <tim@timgthomas.com>
2016-08-10 15:59:54 -0500

    replace monster alert with a modal

--- app/pods/monsters/details/route.js
+++ app/pods/monsters/details/route.js
@@ -23,7 +23,10 @@ export default Ember.Route.extend(DataRoute, {
   },
 
   setupController(controller, model) {
-    controller.set('name', model.get('name'));
+    controller.setProperties({
+      name: model.get('name'),
+      showDeleteConfirmationModal: false
+    });
     this._super(controller, model);
   },
 
@@ -34,12 +37,9 @@ export default Ember.Route.extend(DataRoute, {
       // `attacks` collection to only non-deleted ones cause issues?
       await this.currentModel.save()
       this.currentModel.get('attacks').invoke('save')
-      this.transitionTo('monsters.index');
+      this.transitionTo('monsters.details', this.currentModel.get('id'));
     },
     async delete(monster) {
-      if (!confirm('Are you sure you wish to delete this monster?')) {
-        return;
-      }
       await monster.destroyRecord();
       this.transitionTo('monsters');
     },
--- tests/acceptance/monsters-test.js
+++ tests/acceptance/monsters-test.js
@@ -24,9 +24,11 @@ test('should add a new monster', async function(assert) {
   fillIn('#name', 'New Monster');
   await click('[type=submit]');
 
-  let pcs = await this.store.findAll('monster');
-  assert.equal(pcs.get('length'), 3, 'saved the monster');
+  let monsters = await this.store.findAll('monster');
+  let newMonster = monsters.objectAt(2);
+  assert.equal(monsters.get('length'), 3, 'saved the monster');
   assert.equal(find('.content li').length, 3, 'added the monster to the list');
+  assert.equal(currentURL(), `/monsters/${newMonster.get('id')}`, 'redirected to the new monster');
 
 });
 
@@ -39,5 +41,46 @@ test('should add attacks to a monster', async function(assert) {
   await click('#attacks_add');
 
   assert.equal(find('#attacks tbody tr').length, 1, 'added the attack');
+
+});
+
+test('should confirm when deleting a monster', async function(assert) {
+
+  FactoryGuy.cacheOnlyMode();
+  let monster = make('monster');
+
+  visit(`/monsters/${monster.get('id')}`);
+  await click('button:contains("Delete")');
+
+  assert.equal(find('.modal').length, 1, 'showed the "are you sure?" modal');
+
+});
+
+test('should delete a monster', async function(assert) {
+
+  FactoryGuy.cacheOnlyMode();
+  let monsters = makeList('monster', 2);
+  let monster = monsters.objectAt(0);
+
+  visit(`/monsters/${monster.get('id')}`);
+  click('button:contains("Delete")');
+  await click('form.modal button:contains("Yes")');
+
+  let savedMonsters = await this.store.findAll('monster');
+  assert.equal(savedMonsters.get('length'), 1, 'deleted the monster');
+  assert.equal(currentURL(), '/monsters', 'transitioned back to the list');
+
+});
+
+test('should dismiss the "are you sure?" modal when canceling', async function(assert) {
+
+  FactoryGuy.cacheOnlyMode();
+  let monster = make('monster');
+
+  visit(`/monsters/${monster.get('id')}`);
+  click('button:contains("Delete")');
+  await click('form.modal button:contains("No")');
+
+  assert.equal(find('.modal').length, 0, 'dismissed the "are you sure?" modal');
 
 });
